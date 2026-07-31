f2cb6b6e0c04e684c8ffe40f7946015eac997a87
sbmgtextiles <sbmgtextiles@gmail.com>
2026-07-31 19:20:40 +0530

    Delete data directory

--- data/README.txt
+++
@@ -1,1 +0,0 @@
-Website content files. Use admin.html to export replacements.
--- data/settings.js
+++
@@ -1,8 +0,0 @@
-window.SBMG_SETTINGS = {
-  brand: "SBMG Textiles",
-  whatsapp: "919000000000",
-  phoneDisplay: "+91 90000 00000",
-  email: "sbmgtextiles@gmail.com",
-  address: "Tirupur, Tamil Nadu, India",
-  whatsappMessage: "Hello SBMG Textiles, I would like to enquire about your wholesale kidswear collection."
-};
